fed2d1189b98f693033e7e52111957add3b9eece
raj121276thakre <raj121276thakre@gmail.com>
2024-08-09 20:14:07 +0530

    4. Drawer Design done.

--- app/src/main/java/com/example/astrologermobileapp/MainActivity.kt
+++ app/src/main/java/com/example/astrologermobileapp/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.astrologermobileapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.astrologermobileapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,15 +23,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var toolbarTitle: TextView
 
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Set the status bar color to yellow
+        window.statusBarColor = ContextCompat.getColor(this, R.color.yellow)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            handleNavigationItemSelected(menuItem)
+            true
+        }
+
+       binding.ivMenu.setOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
         }
 
 
@@ -33,6 +62,52 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+    private fun handleNavigationItemSelected(menuItem: MenuItem) {
+        when (menuItem.itemId) {
+//            R.id.nav_dashboard -> {
+//                // Handle Dashboard click
+//            }
+//            R.id.nav_order_history -> {
+//                // Handle Order History click
+//            }
+//            R.id.nav_wallet -> {
+//                // Handle Wallet Transactions click
+//            }
+//            R.id.nav_settings -> {
+//                // Handle Settings click
+//            }
+//            R.id.nav_faq_support -> {
+//                // Handle FAQs & Support click
+//            }
+//            R.id.nav_how_to_use -> {
+//                // Handle How to Use click
+//            }
+//            R.id.nav_rate_us -> {
+//                // Handle Rate Us click
+//            }
+//            R.id.nav_share_app -> {
+//                // Handle Share App click
+//            }
+//            R.id.nav_about_us -> {
+//                // Handle About Us click
+//            }
+        }
+        // Highlight the selected item
+        menuItem.isChecked = true
+        // Close the drawer after handling the click
+        drawerLayout.closeDrawer(GravityCompat.START)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 
     private fun setupBottomNavigation() {
         // Initialize the NavHostFragment and NavController
